c7bfd3e19b7bfd92d88ea7460ecae91e4ad86a86
Chen Guo <ycguo1@gmail.com>
2015-04-07 12:53:59 +1000

    Add print

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * Created by guoch on 7/04/2015.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        try {
+            System.in.read();
+        } catch(Exception e) {
+
+        }
+    }
+
+}
