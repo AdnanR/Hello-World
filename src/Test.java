cda54cc52f8e5043b4fc0f1b03fdb73b7f879738
tabautomation <tabautomation@users.noreply.github.com>
2015-05-05 17:50:05 +1000

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,3 @@
-import java.io.File;
-import java.io.IOException;
-
-import Entity.Address;
-import Entity.Person;
-import Entity.Student;
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.SerializationConfig;
-
 public class Test {
 
     public static void main(String[] args) {
